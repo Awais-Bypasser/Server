be34683af4cd5a8915e7320245d64849c610607d
Awais-Bypasser <167182002+Awais-Bypasser@users.noreply.github.com>
2024-04-21 13:47:46 +0500

    Update Random.py

--- Random.py
+++ Random.py
@@ -8,11 +8,11 @@ for agents in range(10000):
     
     # Oppo device details
     and_oppo = rc(['14', '11', '10'])
-    model_oppo = rc(['CPH2035', 'CPH3987', 'CPH3983','CPH1853','CPH1837', 'CPH1805', 'CPH1809', 'CPH2201', 'CPH2185', 'CPH2161', 'CPH2139','CPH2385','CPH3803','CPH3853','CPH1005'])
+    model_oppo = rc(['CPH2235', 'CPH2251', 'CPH2249','CPH2237','CPH2247', 'CPH2359', 'CPH2357', 'CPH1983', 'CPH2109', 'CPH2089', 'CPH2091','CPH2009','CPH2037','CPH2043','CPH1951'])
     chrome_oppo = f"{str(rr(80, 444))}.0.{str(rr(3000, 6500))}.{str(rr(11, 499))}"
     
     # Infinix device details
-    models_inf = rc(['X267', 'X571', 'X624B', 'X627V', 'X682B','X695C','X695','X6515','X670'])
+    models_inf = rc(['X6823C', 'X6511B', 'X680', 'X5516B', 'X609B','X660','X660B','X652A','X450'])
     ch_inf = f"{str(rr(80, 999))}.0.{str(rr(3200, 6500))}.{str(rr(11, 999))}"
     
     # Facebook Hua device details
@@ -20,10 +20,10 @@ for agents in range(10000):
     
     # Vivo device details
     and_vivo = f"{str(rr(11, 14))}"
-    models_vivo = rc(['V1955A', 'V2146', 'vivo Y66','V2158', 'vivo 1818', 'V2120','V2162A','V2118A', 'V2124', 'vivo 1951','V2203','V2205','V2117'])
+    models_vivo = rc(['vivo NEX A', 'V2143', 'V1922A','V2206', 'V2102', 'V2059','V2148A','V2133A', 'V2066', 'V2050','V2025','V2126','V2028'])
     
     # Realme device details
-    models_re = rc(['RMX1931', 'RMX1971', 'RMX1991','RMX1992','RMX2001', 'RMX2021', 'RMX2030','RMX2050','RMX2063', 'RMX2072', 'RMX2076','RMX2086','RMX2121','RMX2144'])
+    models_re = rc(['RMX1805', 'RMX1821', 'RMX1851','RMX1911','RMX2032', 'RMX1929', 'RMX2103','RMX3231','RMX2063', 'RMX3063', 'RMX3197','RMX3581','RMX3312',RMX3357'])
     buld_re = rc(['QP1A.190711.020', 'QKQ1.190918.001', 'SP1A.210812.016', 'QTG3.200617.002', 'SQ3A.220705.0040', 'RP1A.200720.011', 'UKQ1.230924.001', 'UKQ1.230917.001', 'TP1A.220624.014'])
     ch_re = f"{str(rr(120, 999))}.0.{str(rr(3000, 6500))}.{str(rr(11, 999))}"
     
@@ -31,7 +31,7 @@ for agents in range(10000):
     ad_sam = rc(['10', '7.0', '14', '11'])
     et = rc(['U','U1','F','G','S','N','FN'])
     model_sam_et = f"SM-A705{et}"
-    models_sam = rc([model_sam_et, f"SM-A505{et}", f"SM-J415{et}", f"SM-G981{et}", f"SM-A305{et}", f"SM-G998{et}", f"SM-A326{et}", f"SM-A226{et}", f"SM-A725{et}", f"SM-N970{et}",f"SM-N986{et}",f"SM-A720{et}", 'AMN-LX9','AMN-LX1','AMN-LX2','AMN-LX3','VOG-L29','VOG-L09','VOG-AL00','VOG-TL00','VOG-L04','VOG-AL10','HW-02L','22101316UP','M2012K11AG','LM-Q730N','LMQ620WA','LM-Q620WA','LM-Q620VAB','LMQ620VAB','LM-Q730'])
+    models_sam = rc([model_sam_et, f"SM-A516{et}", f"SM-A525{et}", f"SM-A600{et}", f"SM-A605{et}", f"SM-A606{et}", f"SM-A700{et}", f"SM-A725{et}", f"SM-G887{et}", f"SM-G885{et}",f"SM-A908{et}",f"SM-T295{et}", 'CLT-L29C','CLT-L29','CLT-L09C','CLT-L09','CLT-AL00','CLT-AL01','CLT-TL01','CLT-AL00L','CLT-L04','HW-01K','CLT-L0J','LMX420','LMX420EMW','LM-X420'])
     ch_sam = f"{str(rr(52, 124))}.0.{str(rr(2200, 6500))}.{str(rr(11, 199))}"
     ewg = rc([models_sam,models_re,models_vivo,model_oppo])
     

9cb88e73c4ee8a55d976e08bd7b52e3181080958
Awais-Bypasser <167182002+Awais-Bypasser@users.noreply.github.com>
2024-04-23 17:19:32 +0500

    Update Random.py

--- Random.py
+++ Random.py
@@ -22,5 +22,5 @@ for agents in range(10000):
     u6 = f"Mozilla/5.0 (Linux; Android {str(rr(4,13))}; {models_sam}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome 4.0/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
     u7 = f"Mozilla/5.0 (Linux; Android {models_sam} Build/" + str(random.randint(1000, 9999)) + ".0." + str(random.randint(1, 999)) + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
     
-    ua = rc([u1,u4, u5, u6])
+    ua = rc([u1,u4, u5, u6,u7])
     ugen.append(ua)

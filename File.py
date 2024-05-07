9d0bb0a0ccb152d5334bb1719a9797f34833158d
Awais-Bypasser <167182002+Awais-Bypasser@users.noreply.github.com>
2024-05-07 12:45:49 +0500

    Update File.py

--- File.py
+++ File.py
@@ -28,5 +28,5 @@ for agents in range(10005):
 	ua5 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/66.0.0.71.72;FBBV/84536284;FBDM/{density=1.5,width=480,height=800};FBLC/pl_PL;FBCR/Pix Wireless;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J100VPP;FBSV/4.4.4;nullFBCA/armeabi-v7a:armeabi;]"
 	ua7 = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/Orca-Android;FBAV/51.0.0.64.71;FBBV/132813153;FBDM/{density=3.2,width=1080,height=1920};FBLC/en_GM;FBCR/Republic Wireless;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-A510M;FBSV/7.0;nullFBCA/armeabi-v7a:armeabi;]"
 	ua10 = "[FBAN/FB4A;FBAV/62.0.0.94.88;FBPN/com.facebook.katana;FBLC/en_US;FBBV/315190856;FBCR/KPNNL;FBMF/OPPO;FBBD/OPPO;FBDV/Find 5;FBSV/12.4.8;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.75,width=1440,height=2780};FB_FW/1;]"
-	ua = str(random.choice([ua7,ua1]))  # Add ua6 to the list of choices
-	ugeni.append(ua) # Append the randomly chosen user agent to the ugen list
+	ua = str(random.choice([ua7]))  # Add ua6 to the list of choices
+	ugeni.append(ua7) # Append the randomly chosen user agent to the ugen list

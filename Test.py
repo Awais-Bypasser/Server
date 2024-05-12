9855890edec9d83556edf3dc5a9ee2682a12f0b1
Awais-Bypasser <167182002+Awais-Bypasser@users.noreply.github.com>
2024-05-12 20:01:43 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import random
 ugeni = []
 def ugeni():
+	fbcr = random.choice(["Telenor","fido","MOVO AFRICA","UFONE-PAKTel","Zong","Jazz","SCO","Jio","Vodafone","Airtel","BSNL","MTNL","Grameenphone","Robi","Banglalink","Teletalk","Telkomsel","Indosat Ooredoo","Axiata","Tri","Smartfren","China Mobile","Unicom","Telecom","Satcom","Docomo","Rakuten","IIJmio","Orange","Verizon","AT&T","T-Mobile","Sprint","Vodafone","Telefonica","EE","Orange","Three"])
+	fblc = random.choice(["ja_JP","ex_MX","en_CU","en_US","fr_FR","fa_IR","es_ES","pt_BR","de_DE","it_IT","ja_JP","ko_KR","ru_RU","zh_CN","ar_AE","en_GB"])
 	ua10 = "[FBAN/FB4A;FBAV/62.0.0.94.88;FBPN/com.facebook.katana;FBLC/"+fblc+";FBBV/315190856;FBCR/"+fbcr+";FBMF/OPPO;FBBD/OPPO;FBDV/Find 5;FBSV/12.4.8;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.75,width=1440,height=2780};FB_FW/1;]"
-	return ua10
-ugeni()# Append the randomly chosen user agent to the ugen lis
+	return ua10 # Append the randomly chosen user agent to the ugen lis

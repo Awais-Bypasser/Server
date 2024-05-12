4db490fc5b2b47beaf09569db9145ec128a5656d
Awais-Bypasser <167182002+Awais-Bypasser@users.noreply.github.com>
2024-05-12 19:58:42 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,4 +2,5 @@ import random
 ugeni = []
 def ugeni():
 	ua10 = "[FBAN/FB4A;FBAV/62.0.0.94.88;FBPN/com.facebook.katana;FBLC/"+fblc+";FBBV/315190856;FBCR/"+fbcr+";FBMF/OPPO;FBBD/OPPO;FBDV/Find 5;FBSV/12.4.8;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.75,width=1440,height=2780};FB_FW/1;]"
-	return ua10 # Append the randomly chosen user agent to the ugen lis
+	return ua10
+ugeni()# Append the randomly chosen user agent to the ugen lis
